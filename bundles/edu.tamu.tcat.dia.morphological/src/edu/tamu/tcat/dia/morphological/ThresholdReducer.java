843b6079baf554ea49b43fee7b41a5603c7bb382
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-25 15:46:33 -0600

    Cleaned up ThresholdReduction implementation
    https://issues.citd.tamu.edu/browse/DIA-4

--- bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/morphological/ThresholdReducer.java
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/morphological/ThresholdReducer.java
@@ -1,93 +1,73 @@
 package edu.tamu.tcat.dia.morphological;
 
-import java.util.logging.Logger;
-
 import edu.tamu.tcat.dia.binarization.BinaryImage;
 import edu.tamu.tcat.dia.binarization.BooleanArrayBinaryImage;
 
+/**
+ *  
+ *
+ */
 public final class ThresholdReducer
 {
+   // private final static Logger logger = Logger.getLogger("edu.tamu.tcat.dia.morph.reduction");
 
-   private BinaryImage input;
-   private int scaleFactor;
-   private int threshold;
-   private int width;
-   private int height;
-   private int scaledWidth;
-   private int scaledHeight;
-   private BooleanArrayBinaryImage output;
-   private final static Logger logger = Logger.getLogger("edu.tamu.tcat.dia.morph.reduction");
-
-   public ThresholdReducer(BinaryImage source, int scaleFactor, int threshold)
+   public static BinaryImage threshold(BinaryImage input, int threshold)
    {
-      this.input = source;
+      // TODO enable customizable block size. For now, we'll stick with blocks of 4 pixels
+      int w = makeEven(input.getWidth());
+      int h = makeEven(input.getHeight());
 
-      this.scaleFactor = scaleFactor;
-      this.threshold = threshold;
-
-      this.width = source.getWidth();
-      this.height = source.getHeight();
-
-      if (height % 2 != 0)
-         this.height = height - 1;
-
-      if (width % 2 != 0)
-         this.width = width - 1;
-
-      logger.fine("Input Image Height: " + height + ", Width: " + width);
+      int scaledWidth = (int)(w / 2);
+      int scaledHeight = (int)(h / 2);
       
-      this.scaledWidth = (int)(width / Math.sqrt(scaleFactor));
-      this.scaledHeight = (int)(height / Math.sqrt(scaleFactor));
-      //logger.finest("Scaled Height: " + scaledHeight + ", Scaled Width: " + scaledWidth);
+      BooleanArrayBinaryImage output = new BooleanArrayBinaryImage(scaledWidth, scaledHeight);
 
-      this.output = new BooleanArrayBinaryImage(scaledWidth, scaledHeight);
-   }
-
-   public BinaryImage run()
-   {
-
-      //Each row is processed in blocks of four pixels and stored in scaled image
-      //if sum of values of 4 pixels >= threshold, set output pixel to true
-
-      //TODO - Block size should be changed to a square based on the scale factor
-
-      int outputRowIx = 0;
+      // Each row is processed in blocks of four pixels and stored in scaled image
+      // if sum of values of 4 pixels >= threshold, set output pixel to true
       int outputColIx = 0;
       int offset = 0;
-      for (int rowIx = 0; rowIx < height; rowIx += 2)
+      for (int rowIx = 0; rowIx < h; rowIx += 2)
       {
          outputColIx = 0;
-         for (int colIx = 0; colIx < width; colIx += 2)
+         for (int colIx = 0; colIx < w; colIx += 2)
          {
-
-            int sumValues = 0;
-            if (input.isForeground(colIx, rowIx))
-               sumValues += 1;
-
-            if (input.isForeground(colIx, rowIx + 1))
-               sumValues += 1;
-
-            if (input.isForeground(colIx + 1, rowIx))
-               sumValues += 1;
-
-            if (input.isForeground(colIx + 1, rowIx + 1))
-               sumValues += 1;
-
-            if (sumValues >= threshold)
-            {
+            int ct = countForegroundPixels(input, rowIx, colIx);
+            if (ct >= threshold)
                output.setForeground(offset + outputColIx);
-            }
 
             outputColIx++;
-
          }
+         
          offset += scaledWidth;
-         outputRowIx++;
-
       }
 
-      logger.fine("Output Image Height: " + output.getHeight() + ", width: " + output.getWidth());
       return output;
+   }
+   
+   /**
+    * @return 1 if the x,y coordinates of the image are foreground, 0 otherwise.
+    */
+   private static int asInt(BinaryImage input, int x, int y)
+   {
+      return input.isForeground(x, y) ? 1 : 0;
+   }
+   
+   /**
+    * @return And even number that is equal to {@code value} or {@code value - 1}  
+    */
+   private static int makeEven(int value)
+   {
+      return (value % 2 != 0) ? value - 1 : value;
+   }
+   
 
+   private static int countForegroundPixels(BinaryImage input, int rowIx, int colIx)
+   {
+      int ct =
+            asInt(input, colIx, rowIx) +
+            asInt(input, colIx, rowIx + 1) +
+            asInt(input, colIx + 1, rowIx) +
+            asInt(input, colIx + 1, rowIx + 1);
+      return ct;
    }
 }
---
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/morphological/datatrax/ThresholdReducerTransformer.java
@@ -0,0 +1,87 @@
+package edu.tamu.tcat.dia.morphological.datatrax;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import edu.tamu.tcat.analytics.datatrax.Transformer;
+import edu.tamu.tcat.analytics.datatrax.TransformerConfigurationException;
+import edu.tamu.tcat.analytics.datatrax.TransformerContext;
+import edu.tamu.tcat.dia.binarization.BinaryImage;
+import edu.tamu.tcat.dia.morphological.ThresholdReducer;
+
+public class ThresholdReducerTransformer implements Transformer
+{
+
+   public static final String BINARY_IMAGE_PIN = "binary";
+   
+   public static final String EXTENSION_ID = "tcat.dia.morphological.thresholdreducer";
+   
+   /** The number of times this reduction should be performed. */
+   public static final String PARAM_T = "t";
+   
+   /** The number of times this reduction should be performed. */
+   public static final String PARAM_ITERATIONS = "iterations";
+
+   private int t = 1;
+   private int iterations = 1;
+
+   public ThresholdReducerTransformer()
+   {
+   }
+
+   public void setThreshold(int t)
+   {
+      this.t = t;
+   }
+   
+   public void setNumberOfIterations(int iterations) throws TransformerConfigurationException
+   {
+      if (iterations < 1)
+         throw new TransformerConfigurationException("Invalid number of iterations. Must be at least 1.");
+   
+      this.iterations = iterations;
+   }
+   
+   @Override
+   public void configure(Map<String, Object> config) throws TransformerConfigurationException
+   {
+      if (config.containsKey(PARAM_T))
+      {
+         Integer value = Transformer.getValue(config, PARAM_T, Integer.class);
+         setThreshold(value.intValue());
+      }
+      
+      if (config.containsKey(PARAM_ITERATIONS))
+      {
+         Integer value = Transformer.getValue(config, PARAM_ITERATIONS, Integer.class);
+         setNumberOfIterations(value.intValue());
+      }
+   }
+
+   @Override
+   public Map<String, Object> getConfiguration()
+   {
+      Map<String, Object> config = new HashMap<>();
+      config.put(PARAM_T, Integer.valueOf(t));
+      config.put(PARAM_ITERATIONS, Integer.valueOf(iterations));
+
+      return config;
+   }
+
+   @Override
+   public Callable<?> create(TransformerContext ctx)
+   {
+      final BinaryImage source = (BinaryImage)ctx.getValue(BINARY_IMAGE_PIN);
+      return () -> {
+         BinaryImage output = source;
+         for (int i = 0; i < iterations; i++) 
+         {
+            output = ThresholdReducer.threshold(output, t);
+         }
+         
+         return output;
+      };
+   }
+
+}
