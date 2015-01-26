f33ea1b71ceca9a0e18c51736648d1e72cf0d149
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-25 21:18:23 -0600

    Finished and tested UnionOperator
    https://issues.citd.tamu.edu/browse/VP-9

--- bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/UnionOperator.java
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/UnionOperator.java
@@ -1,8 +1,5 @@
 package edu.tamu.tcat.dia.segmentation.images.bloomberg;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import edu.tamu.tcat.analytics.image.region.Point;
 import edu.tamu.tcat.dia.binarization.BinaryImage;
 import edu.tamu.tcat.dia.binarization.BooleanArrayBinaryImage;
@@ -10,54 +7,61 @@ import edu.tamu.tcat.dia.segmentation.cc.ConnectComponentSet;
 import edu.tamu.tcat.dia.segmentation.cc.ConnectedComponent;
 import edu.tamu.tcat.dia.segmentation.cc.twopass.Finder;
 
+/**
+ * Generates a halftone mask image used to locat illustrations within a document image.
+ * All connected components from a source binary image (a 16x1 sub-sampled version of the 
+ * input image in the original Bloomberg algorithm) will be compared to a supplied seed 
+ * image. All intersecting components will be passed on for subsequent processing.  
+ */
 public final class UnionOperator {
 
-	public static BinaryImage findOverlap(BinaryImage a, BinaryImage b)
-	{
-	   throw new UnsupportedOperationException();
-	}
-
 	/**
+	 * Generates a halftone mask image used to locat illustrations within a document image.
+	 * All connected components from a source binary image (a 16x1 sub-sampled version of the 
+	 * input image in the original Bloomberg algorithm) will be compared to a supplied seed 
+	 * image. All intersecting components will be passed on for subsequent processing.  
 	 * 
-	 * @param initialReduction
-	 * @param intermediate
-	 * @return
+	 * @param source The source image from which to generate the mask image.
+	 * @param seed A seed image constructed to preserve portions of large elements of any 
+	 *      illustrations in the input image (if any illustrations are present). 
+	 * @return A preliminary halftone mask that (ideally) partially covers any illustration 
+	 *      in the input image. This image will be further processed by subsequent steps in 
+	 *      Bloomberg's algorithm.
 	 */
-	public static BinaryImage computeIntersection(BinaryImage initialReduction, BinaryImage intermediate)
+	public static BinaryImage computeMask(BinaryImage source, BinaryImage seed)
    {
-      Finder ccFinder = new Finder(intermediate, 1000);
-      ConnectComponentSet seedComponents = ccFinder.call();
-      ccFinder = new Finder(initialReduction, 10000);
-      ConnectComponentSet sourceCCs = ccFinder.call();
+	   ConnectComponentSet sourceCCs = findComponents(source);
+      ConnectComponentSet seedComponents = findComponents(seed);
       
-//      System.out.println(seedComponents.listLabels().size());
-//      System.out.println(sourceCCs.listLabels().size());
-      Set<ConnectedComponent> union = new HashSet<>();
-//      int ct = 0;
+      BooleanArrayBinaryImage result = new BooleanArrayBinaryImage(source.getWidth(), source.getHeight());
+      if (seedComponents.listLabels().isEmpty())
+         return result;    // this will be the case if the image has no illustrations.
+      
       for (ConnectedComponent sourceCC : sourceCCs)
       {
-         if (sourceCC.getNumberOfPixels() < 5)
-            continue;
-//         ct++;
          for (ConnectedComponent seedCC : seedComponents)    
          {
             if (seedCC.intersects(sourceCC))
-               union.add(sourceCC);
-         }
-      }
-      
-      int w = initialReduction.getWidth();
-      int h = initialReduction.getHeight();
-      BooleanArrayBinaryImage result = new BooleanArrayBinaryImage(w, h);
-      for (ConnectedComponent cc : union)
-      {
-         for (Point p : cc.getPoints())
-         {
-            result.setForeground(p.getY() * w + p.getX());
+               writeComponent(result, sourceCC);
          }
       }
       
       return result;
+   }
 
+   private static ConnectComponentSet findComponents(BinaryImage seed)
+   {
+      Finder ccFinder = new Finder(seed, 10_000);
+      ConnectComponentSet seedComponents = ccFinder.call();
+      return seedComponents;
+   }
+
+   private static void writeComponent(BooleanArrayBinaryImage result, ConnectedComponent cc)
+   {
+      int w = result.getWidth();
+      for (Point p : cc.getPoints())
+      {
+         result.setForeground(p.getY() * w + p.getX());
+      }
    }
 }
