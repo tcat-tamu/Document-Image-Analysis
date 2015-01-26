6bba6cd9249d0128abd073b7d7c7fcdf90dcb6ce
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-25 20:27:35 -0600

    Simplified ExpansionOperator
    https://issues.citd.tamu.edu/browse/DIA-4

--- bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/ExpansionOperator.java
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/ExpansionOperator.java
@@ -1,73 +1,35 @@
 package edu.tamu.tcat.dia.segmentation.images.bloomberg;
 
-import java.util.logging.Logger;
-
 import edu.tamu.tcat.dia.binarization.BinaryImage;
 import edu.tamu.tcat.dia.binarization.BooleanArrayBinaryImage;
 
 public final class ExpansionOperator {
 
-	private BinaryImage input;
-	private int scaleFactor;
-	private int width;
-	private int height;
-	private int scaledWidth;
-	private int scaledHeight;
-	private BooleanArrayBinaryImage output;
-	private final static Logger logger = Logger.getLogger("edu.tamu.tcat.dia.morph.expansion");
+	public static BinaryImage expand(BinaryImage source) 
+	{
+	   int height = source.getHeight();
+      int width = source.getWidth();
+      int scaledWidth = width * 2;
+      int scaledHeight = height * 2;
 
+      BooleanArrayBinaryImage output = new BooleanArrayBinaryImage(scaledWidth, scaledHeight);
+      for (int rowIx = 0; rowIx < height; rowIx++) {
+         int r = rowIx * 2;
 
-	public ExpansionOperator(BinaryImage source, int scaleFactor) {
-		this.input = source;
-
-		this.scaleFactor = scaleFactor;
-		
-		this.width = source.getWidth();
-		this.height = source.getHeight();
-		
-		logger.fine("Expander Input: Height: "+height+", Width: "+width);
-		this.scaledWidth = (int) (width * Math.sqrt(scaleFactor));
-		this.scaledHeight = (int) (height * Math.sqrt(scaleFactor));
-		
-		this.output = new BooleanArrayBinaryImage(scaledWidth, scaledHeight);
-	}
-
-	public BinaryImage run() {
-		
-		int pixelX = 0;
-		int pixelY = 0;
-		for (int rowIx = 0; rowIx < height; rowIx++) {
-			//logger.fine("Processing row: "+rowIx);
-			
-			for (int colIx = 0; colIx < width; colIx++){
-				//logger.finest("Processing col: "+colIx);
-				if(input.isForeground(colIx, rowIx)){
-					//Set foreground for 4 pixels
-					pixelX = rowIx*2;
-					pixelY = colIx*2;
-					//logger.finest("Setting pixelX, pixelY: "+pixelX+","+pixelY+" index: "+(pixelX*scaledWidth+pixelY));
-					output.setForeground(pixelX*scaledWidth+pixelY);
-					pixelX = rowIx*2;
-					pixelY = colIx*2+1;
-					//logger.finest("Setting pixelX, pixelY: "+pixelX+","+pixelY+" index: "+((pixelX*scaledWidth+pixelY)));
-					output.setForeground(pixelX*scaledWidth+pixelY);
-					pixelX = rowIx*2+1;
-					pixelY = colIx*2;	
-					//logger.finest("Setting pixelX, pixelY: "+pixelX+","+pixelY+" index: "+((pixelX*scaledWidth)+pixelY));
-					output.setForeground(pixelX*scaledWidth+pixelY);
-					pixelX = rowIx*2+1;
-					pixelY = colIx*2+1;	
-					//logger.finest("Setting pixelX, pixelY: "+pixelX+","+pixelY+" index: "+((pixelX*scaledWidth)+pixelY));
-					output.setForeground(pixelX*scaledWidth+pixelY);
-					
-				}
-				
-				
-			}
-			
-		}
-		logger.fine("Expanded Height: "+output.getHeight()+", width: "+output.getWidth());
-		return output;
-
+         for (int colIx = 0; colIx < width; colIx++) {
+            
+            if (source.isForeground(colIx, rowIx)) {
+               //Set foreground for 4 pixels
+               int ix = r * scaledWidth + colIx * 2;           // base index
+               output.setForeground(ix);                       // r, c
+               output.setForeground(ix + 1);                   // r, c + 1
+               output.setForeground(ix + scaledWidth);         // r + 1, c
+               output.setForeground(ix + scaledWidth + 1);     // r + 1, c + 1
+               
+            }
+         }
+      }
+      
+      return output;
 	}
 }
--- bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/datatrax/ExpansionTransformer.java
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/segmentation/images/bloomberg/datatrax/ExpansionTransformer.java
@@ -8,20 +8,16 @@ import edu.tamu.tcat.analytics.datatrax.Transformer;
 import edu.tamu.tcat.analytics.datatrax.TransformerConfigurationException;
 import edu.tamu.tcat.analytics.datatrax.TransformerContext;
 import edu.tamu.tcat.dia.binarization.BinaryImage;
-import edu.tamu.tcat.dia.binarization.BooleanArrayBinaryImage;
+import edu.tamu.tcat.dia.segmentation.images.bloomberg.ExpansionOperator;
 
 public class ExpansionTransformer implements Transformer
 {
-
    public final static String EXTENSION_ID = "tcat.dia.morphological.expansion"; 
    public static final String BINARY_IMAGE_PIN = "binary_image";
-   private int scaleFactor = 1;
 
    @Override
    public void configure(Map<String, Object> data) throws TransformerConfigurationException
    {
-      // HACK need to read from config data
-      scaleFactor = 4;
    }
 
    @Override
@@ -36,36 +32,8 @@ public class ExpansionTransformer implements Transformer
    {
       final BinaryImage source = (BinaryImage)ctx.getValue(BINARY_IMAGE_PIN);
       
-      return new Callable<BinaryImage>()
-      {
-         @Override
-         public BinaryImage call() throws Exception
-         {
-            int height = source.getHeight();
-            int width = source.getWidth();
-            int scaledWidth = (int) (width * Math.sqrt(scaleFactor));
-            int scaledHeight = (int) (height * Math.sqrt(scaleFactor));
-            BooleanArrayBinaryImage output = new BooleanArrayBinaryImage(scaledWidth, scaledHeight);
-            
-            for (int rowIx = 0; rowIx < height; rowIx++) {
-               int r = rowIx * 2;
-
-               for (int colIx = 0; colIx < width; colIx++) {
-                  
-                  if (source.isForeground(colIx, rowIx)) {
-                     //Set foreground for 4 pixels
-                     int ix = r * scaledWidth + colIx * 2;           // base index
-                     output.setForeground(ix);                       // r, c
-                     output.setForeground(ix + 1);                   // r, c + 1
-                     output.setForeground(ix + scaledWidth);         // r + 1, c
-                     output.setForeground(ix + scaledWidth + 1);     // r + 1, c + 1
-                     
-                  }
-               }
-            }
-            
-            return output;
-         }
+      return () -> {
+         return ExpansionOperator.expand(source);
       };
    }
 }
