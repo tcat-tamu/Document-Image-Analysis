7efba6160a16f0bc97c081826c61c55fc062f0ee
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-02-17 13:57:03 -0600

    DataTrax transformers for opening, closing and expansion
    https://issues.citd.tamu.edu/browse/VP-9

---
+++ bundles/edu.tamu.tcat.dia.morphological/src/edu/tamu/tcat/dia/morphological/opencv/transformer/OpenCvClosingTransformer.java
@@ -0,0 +1,40 @@
+package edu.tamu.tcat.dia.morphological.opencv.transformer;
+
+import java.util.concurrent.Callable;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.tamu.tcat.analytics.datatrax.TransformerContext;
+import edu.tamu.tcat.dia.morphological.opencv.OpenCvMatrix;
+
+public class OpenCvClosingTransformer extends KernelBasedTransformer 
+{
+   public final static String EXTENSION_ID = "tcat.dia.morphological.opencv.closing"; 
+   public static final String IMAGE_MATRIX_PIN = "image_matrix";
+   
+   public OpenCvClosingTransformer()
+   {
+   }
+   
+   @Override
+   public Callable<OpenCvMatrix> create(TransformerContext ctx)
+   {
+      final OpenCvMatrix input = (OpenCvMatrix)ctx.getValue(IMAGE_MATRIX_PIN);
+      return new Callable<OpenCvMatrix>()
+      {
+         @Override
+         public OpenCvMatrix call() throws Exception
+         {
+            try (OpenCvMatrix kernel = getKernel())
+            {
+               Mat mat = input.get();
+               Mat dest = new Mat(mat.rows(), mat.cols(), mat.type());
+               Imgproc.erode(mat, dest, kernel.get());
+
+               return new OpenCvMatrix(dest);
+            }
+         }
+      };
+   }
+}
